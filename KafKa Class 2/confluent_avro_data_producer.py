1f343ac8c22bce24c33a8a400c7ba00d0a39fd8e
iamabhaydawar <iamabhaydawar@gmail.com>
2025-07-21 17:18:50 -0700

    Add Kafka producer and Avro schema for retail data
    - Implemented a new Python script for producing retail data to Kafka using Avro serialization.
    - Configured Kafka and Schema Registry clients with necessary authentication.
    - Created an Avro schema for the retail data, defining the structure of the records.
    - Added functionality to read data from a CSV file and produce it to a specified Kafka topic with delivery reporting.

---
+++ KafKa Class 2/confluent_avro_data_producer.py
@@ -0,0 +1,91 @@
+import datetime
+import time
+from decimal import *
+from uuid import uuid4,UUID
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import StringSerializer 
+import pandas as pd
+
+
+# Kafka configuration dictionary
+kafka_config = {
+    'bootstrap.servers': 'pkc-57jzz.southcentralus.azure.confluent.cloud:9092',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanisms': 'PLAIN',
+    'sasl.username': 'YOUR_API_KEY',  # Replace with your API key
+    'sasl.password': 'YOUR_API_SECRET'  # Replace with your API secret
+}
+
+# Schema Registry client setup configuration
+schema_registry_client = SchemaRegistryClient(
+    {
+        'url': 'https://psrc-kk5gg.ap-southeast-2.azure.confluent.cloud',
+        'basic.auth.user.info': '{}:{}.format("SPHLZ4VH324C66NP","NekeBJS8t2j9kGea2isP6Y74u8SBnaCiTLIXL5bWRUhjyzVLXkAVP849PNViIiah)'  # Replace with your Schema Registry credentials
+    }
+)
+
+
+#Fetch the latest avro schema for the value
+subject_name='retail_data_dev-value'
+schema_str=schema_registry_client.get_latest_version(subject_name).schema.schema_str
+
+
+
+#Create Avro Serializer for the value
+#key_serializer=AvroSerializer(schema_registry_client, schema_str="type:string")
+# Create serializers
+key_serializer = StringSerializer('utf_8')
+avro_serializer = AvroSerializer(schema_registry_client, schema_str)
+
+#define the SerializingProducer
+producer = SerializingProducer(
+    {
+        'bootstrap.servers': kafka_config['bootstrap.servers'],
+        'security.protocol': kafka_config['security.protocol'],
+        'sasl.mechanisms': kafka_config['sasl.mechanisms'],
+        'sasl.username': kafka_config['sasl.username'],
+        'sasl.password': kafka_config['sasl.password'],
+        'key.serializer': key_serializer, #key will be serialized as a string
+        'value.serializer': avro_serializer, #value will be serialized  as avro
+    }
+)
+
+# Define the delivery report callback
+def delivery_report(err, msg):
+    '''
+    Reports the failure or success of a message delivery.
+
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+    Note:
+         In the delivery report, callback the Message.key() and Message.value()
+         will be the binary format as encoded by any configured Serializers and not the same object that was passed to produce().
+         If you wish to pass the original objects for key and value to delivery report callback, we recommend a bound callback 
+         or lambda where you pass the objects along.
+    '''
+
+    if err is not None:
+        print('Message delivery failed for User record: {}:{}'.format(msg.key(),err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(),msg.topic(),msg.partition(),msg.offset()))
+
+#load the data from the csv file
+df=pd.read_csv('retail_data.csv')
+
+df=df.fillna('null')
+
+#Iterate  over dataframe rows and produce to kafka topic
+for index,row in df.iterrows():
+    #Create a dictionary from the row values
+    data_value=row.to_dict()
+    print(data_value)
+    #Produce the data to the kafka topic
+    producer.produce(topic='retail_data_dev', key=str(index), value=data_value, on_delivery=delivery_report)
+    producer.flush()
+    time.sleep(2)
+    #break
+print("All data successfully published to kafka topic")
